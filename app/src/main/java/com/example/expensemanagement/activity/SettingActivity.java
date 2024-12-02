fb70dd589ff5786991d499aaa5d98b9a885edda9
lamputt <phucvlbh00630@fpt.edu.vn>
2024-12-02 15:09:22 +0700

    switch screen

---
+++ app/src/main/java/com/example/expensemanagement/activity/SettingActivity.java
@@ -0,0 +1,35 @@
+package com.example.expensemanagement.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.expensemanagement.R;
+
+public class SettingActivity extends AppCompatActivity {
+    private ImageView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_setting);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        back = findViewById(R.id.backArrowSettings);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
